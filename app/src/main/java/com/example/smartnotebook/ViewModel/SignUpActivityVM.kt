9dc767dd9e175a41211b26ff19f0c9da7193121b
NikoFan <neoleg2005@gmail.com>
2025-12-17 10:25:02 +0300

    update vm add 'register' method
    Протестировать надо + добавить верификацию по почте на стороне api

--- app/src/main/java/com/example/smartnotebook/ViewModel/SignUpActivityVM.kt
+++ app/src/main/java/com/example/smartnotebook/ViewModel/SignUpActivityVM.kt
@@ -4,10 +4,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.smartnotebook.Model.LoginRequest
+import com.example.smartnotebook.RetrofitActions.ApiFactory
+import kotlinx.coroutines.launch
+import org.json.JSONObject
 
 class SignUpActivityVM : ViewModel() {
 
-    // Прошел ли проверку
+    // Существует ли дубликат нового аккаунта для данных
     private var _isLoginAndPasswordExist by mutableStateOf<Boolean>(true)
     val isLoginAndPasswordExist: Boolean get() = _isLoginAndPasswordExist
 
@@ -17,4 +22,60 @@ class SignUpActivityVM : ViewModel() {
     // Статус ошибки
     private var _statusSignInError by mutableStateOf<String?>(null)
     val statusSignInError: String? get() = _statusSignInError
+
+    // Проверка соответствия почты
+    fun isValidCustomEmail(email: String): Boolean {
+        val regex = Regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")
+        return regex.matches(email)
+    }
+
+    fun registerUserFromDatabase(
+        login: String,
+        password: String,
+        mail: String
+    ){
+        _statusSignInError = null
+        if (login.isEmpty() or password.isEmpty()){
+            _statusSignInError = "Заполните пустые поля"
+            return
+        }
+        if (!isValidCustomEmail(email = mail)){
+            _statusSignInError = "Почта не корректна"
+            return
+        }
+//        viewModelScope.launch {
+//            try {
+//                val request = LoginRequest(
+//                    user_login = login,
+//                    user_password = password)
+//                val response = ApiFactory.apiService.login(
+//                    request = request)
+//
+//                if (response.isSuccessful) {
+//                    val user = response.body()
+//                    // Успешная регистрация — перейти на главный экран
+//                    if (user != null){
+//                        println(user)
+//                        _newUserAccountId = user.user_id.toLong()
+//                    } else {
+//                        _statusSignInError = "Ошибка на сервере"
+//                    }
+//
+//                } else {
+//                    val errorBody = response.errorBody()?.string()
+//                    val errorMessage = try {
+//                        // Парсим JSON вручную или через Gson
+//                        val errorJson = JSONObject(errorBody)
+//                        errorJson.getString("detail") // ← "Invalid password"
+//                    } catch (e: Exception) {
+//                        "Проверьте данные"
+//                    }
+//                    _statusSignInError = errorMessage
+//                }
+//            } catch (e: Exception) {
+//                // Сетевая ошибка, таймаут и т.д.
+//                _statusSignInError = "Ошибка отправки данных"
+//            }
+//        }
+    }
 }
